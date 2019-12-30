e84b51e63d8d9441a1d592bc640c4005085fedd0
KCordilia <k.cordilia@hotmail.com>
2019-12-30 11:07:59 +0100

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Karim Cordilia on 30/12/2019.
+//  Copyright © 2019 Karim Cordilia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
